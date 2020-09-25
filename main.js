bd3692b56d5066d90d8ac14ee7fcf1d6bbe4ad99
keep and carry on <51162782+amhamhamh@users.noreply.github.com>
2020-09-26 00:49:39 +0900

    생활코딩 mysql
    DB TOPIC, ,AUTHOR를 CRUD로 구현

--- main.js
+++ main.js
@@ -1,163 +1,47 @@
-var http = require('http');
-var fs = require('fs');
+var http = require('http');   
 var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var mysql = require('mysql');
-var db = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'111111',
-  database:'opentutorials'
-});
-db.connect();
-
+var topic = require('./lib/topic'); //topic 모듈을 사용함
+var author = require('./lib/author'); //author 모듈을 사용함
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
-      if(queryData.id === undefined){
-        db.query(`SELECT * FROM topic`, function(error,topics){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(topics);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+      if(queryData.id === undefined){  //★★하기의 객체들은 먼저 topic,author에 접근 후 template에 접근     
+        topic.home(request, response);  //topic의 home 객체를 사용-첫 화면
       } else {
-        db.query(`SELECT * FROM topic`, function(error,topics){
-         if(error){
-           throw error;
-         }
-         db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
-           if(error2){
-             throw error2;
-           }
-          var title = topic[0].title;
-          var description = topic[0].description;
-          var list = template.list(topics);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            ` <a href="/create">create</a>
-                <a href="/update?id=${queryData.id}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${queryData.id}">
-                  <input type="submit" value="delete">
-                </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-         })
-      });
+        topic.page(request, response);  //topic의 page 객체를 사용-화면을 읽어옴
       }
-    } else if(pathname === '/create'){
-      db.query(`SELECT * FROM topic`, function(error,topics){
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-          `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    } else if(pathname === '/create_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          db.query(`
-            INSERT INTO topic (title, description, created, author_id) 
-              VALUES(?, ?, NOW(), ?)`,
-            [post.title, post.description, 1], 
-            function(error, result){
-              if(error){
-                throw error;
-              }
-              response.writeHead(302, {Location: `/?id=${result.insertId}`});
-              response.end();
-            }
-          )
-      });
-    } else if(pathname === '/update'){
-      db.query('SELECT * FROM topic', function(error, topics){
-        if(error){
-          throw error;
-        }
-        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
-          if(error2){
-            throw error2;
-          }
-          var list = template.list(topics);
-          var html = template.HTML(topic[0].title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-      });
-    } else if(pathname === '/update_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?', [post.title, post.description, post.id], function(error, result){
-            response.writeHead(302, {Location: `/?id=${post.id}`});
-            response.end();
-          })
-      });
-    } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);       
-          db.query('DELETE FROM topic WHERE id=?', [post.id], function(error, result){
-            if(error){
-              throw error
-            }
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          })
-      });
+    } else if(pathname === '/create'){   //topic의 저서를 생성함.
+      topic.create(request, response);    
+    } else if(pathname === '/create_process'){   //topic의 저서를 처리하는 기능
+      topic.create_process(request, response);
+    } else if(pathname === '/update'){           //topic의 저서를 update하는 기능 
+      topic.update(request, response);
+    } else if(pathname === '/update_process'){  //topic의 저서를 update 처리하는 기능
+      topic.update_process(request, response);
+    } else if(pathname === '/delete_process'){  //topic의 저서를 삭제하는 기능
+      topic.delete_process(request, response);
+    } else if(pathname === '/author'){          //author의 표 형식으로 구현 및 생성하는 기능
+      author.home(request, response);
+    } else if(pathname === '/author/create_process'){ //author 추가처리 하는 기능
+      author.create_process(request, response);         
+    } else if(pathname === '/author/update'){         //author update를 생성하는 기능
+      author.update(request, response);
+    } else if(pathname === '/author/update_process'){ //author update를 처리하는 기능
+      author.update_process(request, response);
+    } else if(pathname === '/author/delete_process'){ //author delete를 삭제하는 기능
+      author.delete_process(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
 });
-app.listen(3000);
+app.listen(3000); 
+
+
+// 해당 index화면은 topic 읽기, topic 생성, topic 생성처리, topic 수정, topic 수정처리
+// author 읽기, author 생성, author 생성처리, author 수정, author 수정처리
+//topic.js나 author.js나 기능을 구현했을 떄, 똑같이 처리해야 되기 때문에
+// 1.페이지 제목 2. 본문 제목 3. 리스트 4. authorselect or authortable 5. 컨트롤 구현은 동일하다. 
